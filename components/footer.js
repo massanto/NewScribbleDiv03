a4d6db9b0f9d207e1728b8c917ce541451bca674
massanto <ehrusan@gmail.com>
2023-03-01 21:02:18 +0700

    first commit

---
+++ components/footer.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export default function Footer({ events }) {
+  return (
+    <footer className="w-full my-8">
+      <div className="text-center lil-text mt-8">
+        <div className="inline-block py-3 px-4 border bg-gray-200 rounded-lg">
+          🍿 Draw Your Imagination.
+        </Link>
+        .
+      </div>
+    </div>
+
+    </footer >
+  );
+}
